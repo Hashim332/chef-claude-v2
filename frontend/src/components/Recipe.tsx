ac91395f13610ef2cb3bd9d971a414b5938a2174
Hashim332 <ppyhr5@nottingham.ac.uk>
2025-05-02 16:12:27 +0100

    setup tailwind typography, wrapped recipe in prose container

--- frontend/src/components/Recipe.tsx
+++ frontend/src/components/Recipe.tsx
@@ -1,14 +1,24 @@
 import { useRecipeContext } from "@/context/HomeContext";
 import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 export default function Recipe() {
   const { recipe } = useRecipeContext();
-  console.log(recipe.text);
-  return (
-    <div className="max-w-2xl mx-auto my-8">
-      <h1 className="text-3xl my-4 mr-auto">Recipe:</h1>
 
-      <Markdown>{recipe.text}</Markdown>
+  if (recipe.text === "" || !recipe.text) {
+    return <div>Loading recipe...</div>;
+  }
+
+  const recipeExists = recipe.text !== "" && recipe.text;
+  const random = "# Hi, *Pluto*!";
+
+  return (
+    <div className="max-w-2xl mx-auto my-8 prose">
+      <h1 className="text-3xl my-4 mr-auto">Recipe:</h1>
+      {recipeExists && (
+        <Markdown remarkPlugins={[remarkGfm]}>{recipe.text}</Markdown>
+      )}
+      <Markdown>{random}</Markdown>
     </div>
   );
 }
