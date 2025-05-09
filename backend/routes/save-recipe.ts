98707e2f85c6e4675e37708f6af8ab4cc6a4ca71
Hashim332 <ppyhr5@nottingham.ac.uk>
2025-05-09 17:33:57 +0100

    implemented proper db subcollection for unique recipe documents

--- backend/routes/save-recipe.ts
+++ backend/routes/save-recipe.ts
@@ -1,7 +1,16 @@
 import express from "express";
 import { db } from "../src/firebase";
-import { doc, updateDoc, arrayUnion, setDoc, getDoc } from "firebase/firestore";
+import {
+  doc,
+  updateDoc,
+  arrayUnion,
+  setDoc,
+  addDoc,
+  getDoc,
+  collection,
+} from "firebase/firestore";
 import { requireAuth, getAuth } from "@clerk/express";
+import { Recipe } from "./by-ingredients";
 
 const router = express.Router();
 
@@ -9,12 +18,12 @@ router.use(express.json());
 
 router.post("/save-recipe", requireAuth(), async (req, res) => {
   const { userId } = getAuth(req);
-  //   const recipe = req.body;
-  const recipe = {
-    recipeName: "test recipe",
-    recipeSummary: "test summary",
-    fullRecipe: "test full recipe",
-  };
+  const recipe = req.body;
+  //   const recipe = {
+  //     recipeName: "test recipe",
+  //     recipeSummary: "test summary",
+  //     fullRecipe: "test full recipe",
+  //   };
 
   if (!userId || !recipe) {
     res.status(400).json({ error: "userId and recipe are required" });
@@ -25,14 +34,17 @@ router.post("/save-recipe", requireAuth(), async (req, res) => {
     const userRef = doc(db, "users", userId);
     const userSnap = await getDoc(userRef);
 
-    if (userSnap.exists()) {
-      await updateDoc(userRef, recipe);
-    } else {
-      await setDoc(userRef, {
-        recipe: recipe,
-      });
-      console.log("recipe saved!!!");
-    }
+    const saveRecipe = async (userId: any, recipeData: Recipe) => {
+      // 1. Point at the subcollection
+      const recipesCol = collection(db, "users", userId, "recipes");
+
+      // 2. Add the document — returns a ref with the new ID
+      const docRef = await addDoc(recipesCol, recipeData);
+
+      console.log("New recipe ID:", docRef.id);
+    };
+
+    saveRecipe(userId, recipe);
 
     res.status(200).json({ message: "Recipe saved successfully" });
   } catch (err) {
